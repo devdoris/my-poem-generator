7743b1b2e3584c64f0ff61fdd0cb32b34f19c9d6
devdoris <devdoris1@gmail.com>
2025-06-15 17:04:13 +0100

    Improved the User Experience.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ function generatePoem(event) {
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
